7b3da4c405e8a6b74b2b6d41d9191f0b24010067
Jakob Sailer <sailer.jakob@web.de>
2022-11-27 18:50:22 +0100

    added camera documentation

--- GameEngine/Rendering/Camera.cs
+++ GameEngine/Rendering/Camera.cs
@@ -4,12 +4,25 @@ using Microsoft.Xna.Framework;
 namespace GameEngine.Rendering;
 
 public class Camera {
+    /// <summary>
+    /// The cameras transform matrix.
+    /// </summary>
     public Matrix Transform { get; protected set; } = Matrix.Identity;
 
+    /// <summary>
+    /// Transforms a given point from global coordinates (literal application window) to local coordinates (the screen the camera is in).
+    /// </summary>
+    /// <param name="global">The point to transform.</param>
+    /// <returns>A point relative to the cameras screen.</returns>
     public Vector2 GlobalToLocal(Vector2 global) {
         return Vector2.Transform(global, Transform);
     }
 
+    /// <summary>
+    /// Transforms a given point from local coordinates (the screen the camera is in) to global coordinates (literal application window).
+    /// </summary>
+    /// <param name="local">The point to transform.</param>
+    /// <returns>A point relative to the applications window.</returns>
     public Vector2 LocalToGlobal(Vector2 local) {
         return Vector2.Transform(local, Matrix.Invert(Transform));
     }
